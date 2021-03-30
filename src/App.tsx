74ac53ff1c2a32f2690d332f00a0101db33ec4e2
tg-developer <tg-developer@gmail.com>
2021-03-30 10:20:02 -0300

    Criação Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import { GlobalStyle } from './styles/global';
+import {Header} from './components/Header'
 
  export function App() {
   return (
-    <div className="App">
-      <h1>Teste 2</h1>
-    </div>
+    <>
+    <Header />
+    <GlobalStyle />
+    </>
   );
-}
-
+} 
+ 
 
