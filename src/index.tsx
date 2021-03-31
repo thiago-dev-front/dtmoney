9fcdd5859371fa867e2b563db48618e2b001b607
tg-developer <tg-developer@gmail.com>
2021-03-30 21:31:40 -0300

    Axios + MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import {createServer} from 'miragejs'
 import {App} from './App'
 
-
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 ReactDOM.render(
   <React.StrictMode>
     <App />
