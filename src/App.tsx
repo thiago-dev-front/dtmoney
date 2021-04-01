cbcd7732160885ee9f78b43048f9c91ca65652c1
tg-developer <tg-developer@gmail.com>
2021-03-31 22:29:08 -0300

    Subida Moddal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import * as React from 'react';
-import { GlobalStyle } from './styles/global';
-import {Header} from './components/Header'
-import {Dashboard} from './components/Dashboard'
 
+import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
+import {Header} from './components/Header';
+import {Dashboard} from './components/Dashboard';
+import { useState } from 'react';
+import {NewTransactionModal} from './components/NewTransactionModal';
+
+ Modal.setAppElement('#root')
  export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTrasactionModalOpen ] = useState(false);
+
+  function handleOpenNewTrasactionModal() {
+    setIsNewTrasactionModalOpen(true)
+  }
+
+  function handleCloseNewTrasactionModal() {
+    setIsNewTrasactionModalOpen(false)
+  }
+  
   return (
     <>
-    <Header />
+    <Header onOpenNewTransactionModal={handleOpenNewTrasactionModal}/>
     <Dashboard />
+    <NewTransactionModal 
+     isOpen={isNewTransactionModalOpen}
+     onRequestClose={handleCloseNewTrasactionModal}
+     />
     <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,13 @@
+import Modal from "react-modal"
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+export function NewTransactionModal({isOpen,onRequestClose }: NewTransactionModalProps) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <h2>Cadastrar promoção</h2>
+</Modal>
+  )
+}
